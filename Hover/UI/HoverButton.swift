33060e3b379ed22ac1ffa405f2e0afc3e1617f28
Mack <mack.cross@hotmail.com>
2021-01-15 20:52:49 +1300

    add 'highlightView' in uikit button

--- Hover/UI/HoverButton.swift
+++ Hover/UI/HoverButton.swift
@@ -83,6 +83,7 @@ private extension HoverButton {
         let hostingController = UIHostingController(rootView: swiftUIButton)
         
         add(views: hostingController.view)
+        add(views: hightlightView)
     }
     
     func addSubviews() {
